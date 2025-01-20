a4b42f771bfcbd4d337edb2b42b487ebc07cdc26
zhangzhongyou <zhangzhongyou@bytedance.com>
2025-01-20 10:53:36 +0800

    feat: 问题修改

---
+++ src/app/(home)/layout.tsx
@@ -0,0 +1,26 @@
+import { FC, PropsWithChildren } from "react";
+import Image from "next/image";
+import Link from "next/link";
+const HomeLayout: FC<PropsWithChildren<any>> = ({ children }) => {
+  return (
+    <div className="w-full h-full flex flex-col">
+      <div className="w-full flex items-center justify-between px-24 py-16 shadow">
+        <Image src={"/web/logo.png"} width={100} height={24} alt="logo" />
+        <div className="space-x-8 flex items-center">
+          <Link href={"/register"}>
+            <div className="px-16 py-8 border-1 border-solid border-222222 text-xs14 font-medium text-222222 rounded-16 cursor-pointer">
+              Log Up
+            </div>
+          </Link>
+          <Link href={"/login"}>
+            <div className="px-16 py-9 text-xs14 font-medium text-222222 rounded-16 bg-D0FF71 cursor-pointer">
+              Log In
+            </div>
+          </Link>
+        </div>
+      </div>
+      {children}
+    </div>
+  );
+};
+export default HomeLayout;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,11 @@
 import type { Config } from "tailwindcss";
-
+import scrollbar from "tailwind-scrollbar";
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/server-components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -12,7 +13,91 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontSize: ({ theme }: any) => ({
+        ...theme("spacing"),
+      }),
+      lineHeight: ({ theme }: any) => ({
+        ...theme("spacing"),
+      }),
+    },
+    spacing: Array.from({ length: 1000 }).reduce((map: any, _, index) => {
+      map[index] = `${index}px`;
+      return map;
+    }, {}) as any,
+    borderRadius: Array.from({ length: 1000 }).reduce((map: any, _, index) => {
+      map[index] = `${index}px`;
+      return map;
+    }, {}) as any,
+    borderWidth: Array.from({ length: 1000 }).reduce((map: any, _, index) => {
+      map[index] = `${index}px`;
+      return map;
+    }, {}) as any,
+    backgroundSize: {
+      "100%": "100% 100%",
+    },
+    colors: {
+      "222222": "#222222",
+      FFFFFF: "#FFFFFF",
+      D0FF71: "#D0FF71",
+      101010: "#101010",
+      EDEDED: "#EDEDED",
+      FADB14: "#FADB14",
+      E8E8E9: "#E8E8E9",
+      "8A8C90": "#8A8C90",
+      FA3100: "#FA3100",
+      FAFAFB: "#FAFAFB",
+      "6F6F6F": "#6F6F6F ",
+      "161B22": "#161B22 ",
+      "4F5357": "#4F5357 ",
+      o232: "rgba(232,232,233,0.2)",
+      o16: "rgba(16,16,16,0.65)",
+      o34: "rgba(34,34,34,0.65)",
+      o348: "rgba(34,34,34,0.8)",
+      F0F0F0: "#F0F0F0",
+    },
+    fontSize: {
+      xs12: [
+        "12px",
+        {
+          lineHeight: "18px",
+        },
+      ],
+      xs14: [
+        "14px",
+        {
+          lineHeight: "21px",
+        },
+      ],
+      xs16: [
+        "16px",
+        {
+          lineHeight: "25px",
+        },
+      ],
+      xs18: [
+        "18px",
+        {
+          lineHeight: "27px",
+        },
+      ],
+      xs20: [
+        "20px",
+        {
+          lineHeight: "30px",
+        },
+      ],
+      xs26: [
+        "26px",
+        {
+          lineHeight: "39px",
+        },
+      ],
+    },
+    screens: {
+      desktop12: "1280px",
+      desktop14: "1440px",
+      desktop19: "1920px",
     },
   },
-  plugins: [],
+  plugins: [scrollbar],
 } satisfies Config;
