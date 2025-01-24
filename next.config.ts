c1d65118e4242b7f3c4497d838b0ee423e5d640c
zhangzhongyou <zhangzhongyou@bytedance.com>
2025-01-24 16:27:52 +0800

    feat: 新增需求

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: process.env.NODE_ENV === "production" ? "/web/" : "",
+  basePath: process.env.NODE_ENV === "production" ? "/web" : "",
 };
 
 export default nextConfig;
